3ec0b11d4941957957a8e25c6964ac1291616616
EC2 Default User <ec2-user@ip-172-31-80-166.ec2.internal>
2023-10-12 16:34:57 +0000

    setup server to run in https mode

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import { MongoClient } from 'mongodb';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import 'dotenv/config';
+import https from 'https';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -42,7 +44,16 @@ app.post('/api/removeBicycle', async (req, res) => {
   res.json(data);
 })
 
+
+const httpsServer = https.createServer({
+	key: fs.readFileSync('/etc/letsencrypt/live/bicyclecollection.xyz/privkey.pem'),
+	cert: fs.readFileSync('/etc/letsencrypt/live/bicyclecollection.xyz/fullchain.pem')}, app);
+
+httpsServer.listen(443, ()=> {
+	console.log('HTTPS Server running on port 443')
+});
+/*
 app.listen(port, () => {
 
   console.log(`Example app listening on port ${port}`)
-})
+})*/
